a8b8e9ad65a3f88910692ef1609b488437d070cf
Zeta <zetag174@gmail.com>
2024-10-10 21:34:18 -0600

    Make Login and Home Screens

--- lib/presentation/widgets/card.dart
+++ lib/presentation/widgets/card.dart
@@ -1,47 +1,114 @@
 import 'package:flutter/material.dart';
+
 class CardCustom extends StatelessWidget {
   const CardCustom({super.key});
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Container(
-      width: (size.height * 0.5)-37,
-      height: size.height * 0.2,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.grey,
-            offset: Offset(0, 3),
-            blurRadius: 6,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 340,
+        height: 220,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: const LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Color.fromARGB(255, 0, 255, 98),Color.fromARGB(255, 0, 215, 83), Color.fromARGB(255, 40, 198, 0)],
           ),
-        ],
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 8,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'Crédito',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Mastercard-logo.svg/1280px-Mastercard-logo.svg.png',
+                  height: 50,
+                  width: 80,
+                ),
+              ],
+            ),
+           
+            const Text(
+              '5412 7512 3412 3456',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+                letterSpacing: 2,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'TITULAR DE LA TARJETA',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 10,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      'JUAN PÉREZ',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'VENCE',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 10,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      '12/25',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-      child: const Column(
-        children: <Widget>[
-          Padding(
-            padding:  EdgeInsets.all(8.0),
-            child: Text(
-              'Hola "Name"',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Padding(
-            padding:  EdgeInsets.all(8.0),
-            child: Text(
-              'Tu saldo actual es de: \$0.00',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ], 
-      )
-    );
-  }
+    );}
 }
